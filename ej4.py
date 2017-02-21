b239a611fbd72a827a556d1a1d95e26cee4c222e
Juan Jose <jjlr1994@gmail.com>
2017-02-21 16:03:43 +0100

    Modificacion del cuarto ejercicio

--- ej4.py
+++ ej4.py
@@ -9,3 +9,6 @@ horario=raiz.find("biblioteca/horario")
 for biblioteca in raiz:
     if bi in biblioteca.find("nombre").text:
         print "La Biblioteca se llama:",biblioteca.find("nombre").text
+        print "Tiene un horario de: "
+        for hora in horario:
+            print "Dia",hora.tag,"tiene un horario de ",hora.text
