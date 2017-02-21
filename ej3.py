cc1bd64438eb06206ae8f2b37e42caeb33e5f12b
Juan Jose <jjlr1994@gmail.com>
2017-02-21 16:17:09 +0100

    Introduccion del enunciado en el tercer ejercicio

--- ej3.py
+++ ej3.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+#Introduce por teclado una letra y que muestre las Bibliotecas que coinciden con esos caracteres introducidos.
 from lxml import etree
 doc = etree.parse('Biblioteca.xml')
 raiz=doc.getroot()
