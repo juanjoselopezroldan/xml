0911e49c64b562cc7bac1a203b25e1bff3d1010c
Juan Jose <jjlr1994@gmail.com>
2017-02-21 16:06:20 +0100

    Modificacion del cuarto ejercicio

--- ej5.py
+++ ej5.py
@@ -10,3 +10,4 @@ for codigo in raiz:
         print "-La Biblioteca se llama:",codigo.find("nombre").text
         print "-Tiene la siguiente direccion:",codigo.find("direccion").text
         print "-Informacion sobre la Biblioteca:",codigo.find("descripcion").text
+        print "Su URL es la siguiente: http://www.openstreetmap.org/#map=19/",codigo.find("latitud").text,"/",codigo.find("longitud").text
