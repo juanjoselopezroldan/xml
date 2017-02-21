05cbe57bd94014020759176a5e114463a41e02eb
Juan Jose <jjlr1994@gmail.com>
2017-02-21 16:18:11 +0100

    Introduccion del enunciado en el quinto ejercicio

--- ej5.py
+++ ej5.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+#Introduce por teclado el codigo de la biblioteca y que muestre el nombre, la direccion, la descripcion del mismo y sus coordenadas en una url para poder acceder a la ubicacion.
 from lxml import etree
 doc = etree.parse('Biblioteca.xml')
 raiz=doc.getroot()
