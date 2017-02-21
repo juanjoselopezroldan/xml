b5c8ecf9a71c6fd85a6b2e213081337bea82ca68
Juan Jose <jjlr1994@gmail.com>
2017-02-21 16:05:57 +0100

    Modificacion del cuarto ejercicio

--- ej5.py
+++ ej5.py
@@ -8,3 +8,4 @@ bi=raw_input("Introduce la ID: ")
 for codigo in raiz:
     if codigo.find("idbiblio").text==bi:
         print "-La Biblioteca se llama:",codigo.find("nombre").text
+        print "-Tiene la siguiente direccion:",codigo.find("direccion").text
