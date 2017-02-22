3782dd3c762a7f01643b87d0f0abbaf7313e58c4
juan jose lopez <jjlr1994@gmail.com>
2017-02-22 12:13:08 +0100

    Mod del ejercicio 5

--- ej5.py
+++ ej5.py
@@ -3,13 +3,23 @@
 from lxml import etree
 doc = etree.parse('Biblioteca.xml')
 raiz=doc.getroot()
+horario=raiz.find("biblioteca/horario")
 
-dia=raw_input("Introduce el dia: ")
 
+dia=raw_input("Introduce un dia de la semana: ")
+hora=int(raw_input("Introduce la hora 24H para ver que biblioteca esta abierta: "))
+no="false"
 for cod in raiz:
     for codigo in cod:
         for dias in codigo:
-            if dias.tag==dia:
+            lista=dias.text.split("-")
+            horain=int(lista[0].split(":")[0])
+            horafin=int(lista[1].split(":")[0])
+            if dias.tag==dia and horain<=hora and horafin>=hora:
                 print "-La Biblioteca se llama:",codigo.getparent().find("nombre").text
                 print "-Tiene la siguiente direccion:",codigo.getparent().find("direccion").text
                 print "Su URL es la siguiente: http://www.openstreetmap.org/#map=19/",codigo.getparent().find("latitud").text,"/",codigo.getparent().find("longitud").text
+                print 10*"-"
+                no="true"
+if no=="false":
+    print "No hay ninguna biblioteca que habra ese dia a esa hora"
