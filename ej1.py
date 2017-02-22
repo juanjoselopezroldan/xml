afbe93be61b0b33e15660126066349b0863a777c
juan jose lopez <jjlr1994@gmail.com>
2017-02-22 12:08:16 +0100

    Modificacion del ejercicio1

--- ej1.py
+++ ej1.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
-#Lista la cantidad de Bibliotecas existentes.
+#Lista las Bibliotecas.
 from lxml import etree
 doc = etree.parse('Biblioteca.xml')
 raiz=doc.getroot()
 
-contador=0
-
 for biblio in raiz:
-    contador=contador+1
-
-print "La cantidad de bibliotecas es de: ",contador
+    print biblio.find("nombre").text
